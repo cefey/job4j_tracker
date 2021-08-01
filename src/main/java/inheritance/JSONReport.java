27a4c6cc541c81b146fb90a117e731f6db5201a4
cefey <staffa261@gmail.com>
2021-08-01 21:57:50 +0300

    4. Переопределение [#174890]

---
+++ src/main/java/inheritance/JSONReport.java
@@ -0,0 +1,9 @@
+package inheritance;
+
+public class JSONReport extends TextReport {
+    public String generate(String name, String body) {
+        return "{" + System.lineSeparator() + "   \"name\" : " + "\"" + name + "\""
+               + System.lineSeparator() + "   \"body\" : " + "\"" + body + "\""
+               + System.lineSeparator() + "}";
+    }
+}

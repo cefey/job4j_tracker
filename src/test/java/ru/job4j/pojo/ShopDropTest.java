c189b3a8835a8e103065c1824dd3b23d53857390
cefey <staffa261@gmail.com>
2021-09-16 23:10:42 +0300

    3.2. Удаление моделей из массива. [#310016]

--- src/test/java/ru/job4j/pojo/ShopDropTest.java
+++ src/test/java/ru/job4j/pojo/ShopDropTest.java
@@ -7,6 +7,17 @@ import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
 public class ShopDropTest {
+
+   @Test
+    public void whenDropLast() {
+        Product[] products = new Product[2];
+        products[0] = new Product("Milk", 10);
+        products[1] = new Product("Bread", 4);
+        Product[] rsl = ShopDrop.delete(products, 1);
+        assertThat(rsl[0].getName(), is("Milk"));
+        assertThat(rsl[1], is(nullValue()));
+    }
+
     @Test
     public void whenDropFirst() {
         Product[] products = new Product[2];
@@ -14,16 +25,6 @@ public class ShopDropTest {
         products[1] = new Product("Bread", 4);
         Product[] rsl = ShopDrop.delete(products, 0);
         assertThat(rsl[0].getName(), is("Bread"));
-        assertThat(rsl[1], is(nullValue()));
-    }
-
-    @Test
-    public void whenDropLast() {
-        Product[] products = new Product[2];
-        products[0] = new Product("Milk", 10);
-        products[1] = new Product("Bread", 4);
-        Product[] rsl = ShopDrop.delete(products, 1);
-        assertThat(rsl[0].getName(), is("Milk"));
         assertThat(rsl[1], is(nullValue()));
     }
 

bb05715c0c5372f30ea2366520f695aa701af20d
cefey <staffa261@gmail.com>
2021-10-17 23:41:26 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,8 @@
 package ru.job4j.tracker;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -22,4 +25,22 @@ public class Tracker {
         }
         return rsl;
     }
+
+   /*получение списка всех заявок*/
+    public Item[] findAll() {
+        return  Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] first = findAll();
+        Item[] rls = new Item[size];
+        int inc = 0;
+        for (int i = 0; i < first.length; i++) {
+            if (first[i].getName().equals(key)) {
+                rls[inc++] = first[i];
+            }
+        }
+        return  Arrays.copyOf(rls, inc);
+    }
+
 }

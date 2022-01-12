e9e7da55119cff5837266eed6b9baf9ec568f32a
cefey <staffa261@gmail.com>
2022-01-12 20:54:34 +0300

    0.1. java.lang.NullPointerException [#219364] Исправленный

--- src/main/java/ru.job4j.ex/Fact.java
+++ src/main/java/ru.job4j.ex/Fact.java
@@ -7,10 +7,10 @@ public class Fact {
 
     public int calc(int n) {
         if (n < 0) {
-            throw new IllegalArgumentException("The argument must be greater than zero.");
+            throw new IllegalArgumentException("The argument must be greater than or equal to zero");
         }
         int rsl = 1;
-        for (int index = 1; index <= n; index++) {
+        for (int index = 2; index <= n; index++) {
             rsl *= index;
         }
         return rsl;

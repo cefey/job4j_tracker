ba656af7da653a7f41e28d799ea631a0998da7c2
cefey <staffa261@gmail.com>
2022-05-10 21:51:07 +0300

    5. Вызов переопределенного метода родителя - super.method ... [#504871]

---
+++ src/main/java/inheritance/PizzaExtraCheeseExtraTomato.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class PizzaExtraCheeseExtraTomato extends PizzaExtraCheese {
+
+    @Override
+    public String name() {
+        return super.name() + " + extra tomato";
+    }
+}
+
